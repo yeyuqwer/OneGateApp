905b4074ec3399c5eb31f2bc91b8f1dbe4504d5d
Erik Zhang <erik@neo.org>
2026-04-20 22:36:53 +0800

    Allow copy when exporting keys
    Fix #8

--- OneGateApp/Platforms/Android/MainApplication.cs
+++ OneGateApp/Platforms/Android/MainApplication.cs
@@ -1,10 +1,27 @@
 ﻿using Android.App;
 using Android.Runtime;
+using Microsoft.Maui.Handlers;
 
 namespace NeoOrder.OneGate.Platforms.Android;
 
 [Application]
 public class MainApplication(IntPtr handle, JniHandleOwnership ownership) : MauiApplication(handle, ownership)
 {
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp()
+    {
+        EditorHandler.Mapper.AppendToMapping("ReadOnlyCopyable", (handler, view) =>
+        {
+            if (view.IsReadOnly)
+            {
+                var editText = handler.PlatformView;
+                editText.SetTextIsSelectable(true);
+                editText.ShowSoftInputOnFocus = false;
+                editText.Focusable = true;
+                editText.FocusableInTouchMode = true;
+                editText.Clickable = true;
+                editText.LongClickable = true;
+            }
+        });
+        return MauiProgram.CreateMauiApp();
+    }
 }
